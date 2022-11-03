5408e0471487165450b41edccbd0301438429082
nickhepp <nickhepp@gmail.com>
2022-11-02 19:04:30 -0500

    Adding a logo splash screen that links to the ecs home page on click.

--- PC/HostApp/HostApp/Properties/Resources.Designer.cs
+++ PC/HostApp/HostApp/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HostApp.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -106,6 +106,16 @@ namespace HostApp.Properties {
         internal static System.Drawing.Bitmap construction {
             get {
                 object obj = ResourceManager.GetObject("construction", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap edpf_splash_screen_logo {
+            get {
+                object obj = ResourceManager.GetObject("edpf_splash_screen_logo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
